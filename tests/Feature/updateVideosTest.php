c02230a8137148b1e531a9381269f454fa69edb6
kevinchen <kevin.chen@contin-global.com>
2022-05-13 15:40:07 +0800

    新增updateVideos Test

---
+++ tests/Feature/updateVideosTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class updateVideosTest extends TestCase
+{
+    protected $videoController;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->videoController = $this->app->make('App\Http\Controllers\VideoController');
+    }
+
+    public function test_example()
+    {
+        $response = $this->videoController->updateVideos();
+        $this->assertTrue(true);
+    }
+}
